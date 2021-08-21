456f7c7bc27bc5e47095394010d16644d9b71b0a
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-08-21 22:45:00 +0600

    added service in server

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-
 const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 // console.log(process.env.DB_USER);
 // console.log(process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2uohe.mongodb.net/perfectClick?retryWrites=true&w=majority`;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(bodyParser.json());
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("perfectClick").collection("bookings");
+    const serviceCollection = client.db("perfectClick").collection("bookings");
     console.log('DB connected successfully');
-    client.close();
+
+    app.post('/addService', (req, res) => {
+        const newService = req.body;
+        console.log('adding new service: ', newService);
+        serviceCollection.insertOne(newService)
+            .then(result => {
+                console.log('inserted count ', result.insertedCount)
+                res.send(result.insertedCount > 0)
+            })
+    })
+
 });
 
 
