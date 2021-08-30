b39d4bdd2d9008bd670ae5c93957d760b3ab5472
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-08-30 15:42:19 +0600

    display servicedetails in serviceinformation & checkout page

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
 const express = require('express');
 const app = express();
 const {MongoClient} = require('mongodb');
-const ObjectID = require('mongodb').ObjectID;
-const bodyParser = require('body-parser');
+const ObjectID = require('mongodb').ObjectId;
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2uohe.mongodb.net/perfectClick?retryWrites=true&w=majority`;
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     console.log('DB connected successfully');
     const serviceCollection = client.db("perfectClick").collection("services");
 
+    app.get('/', (req, res) => {
+        res.send('Welcome to Perfect Click server!')
+    })
+
     // add services in server
     app.post('/addService', (req, res) => {
         const newService = req.body;
-        // console.log('adding new service: ', newService);
         serviceCollection.insertOne(newService)
             .then(result => {
-                // console.log('inserted count ', result.insertedCount)
                 res.send(result.insertedCount > 0)
             })
     })
 
-    // display services from server
+    // display all services from server
     app.get('/services', (req, res) => {
         serviceCollection.find()
             .toArray((err, services) => {
                 res.send(services)
-                // console.log('from database', services)
             })
     })
 
-    // display service information in ui
-    // app.get('/service/:id', (req, res) => {
-    //     const id = ObjectID(req.params.id);
-    //     serviceCollection.find({_id: id})
-    //         .toArray((err, services) => {
-    //             // res.send(service[0])
-    //             console.log('from database', services[0])
-    //         })
-    // })
+    // display service details in serviceInformation page 
+    app.get('/service/:id', (req, res) => {
+        const id = ObjectID(req.params.id);
+        serviceCollection.find({_id: id})
+            .toArray((err, service) => {
+                res.send(service[0])
+            })
+    })
+
+    // display service information in Checkout page
+    app.get('/service/:id', (req, res) => {
+        const id = ObjectID(req.params.id);
+        serviceCollection.find({_id: id})
+            .toArray((err, service) => {
+                res.send(service[0])
+            })
+    })
 
 });
 
-
-app.get('/', (req, res) => {
-    res.send('Welcome to Perfect Click server!')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
